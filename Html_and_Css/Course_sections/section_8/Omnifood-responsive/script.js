af0d4eba750a882bdb94163321ce71e7a6c81291
kavinesh <kavineshkavinesh860@gmail.com>
2024-06-19 16:51:40 +0530

    Course notes and stuffs

--- Html_and_Css/Course_sections/section_8/Omnifood-responsive/script.js
+++ Html_and_Css/Course_sections/section_8/Omnifood-responsive/script.js
@@ -1,12 +1,93 @@
-const closeMenu = document.querySelector(
-  '.icon-mobile-nav[name="close-outline"]'
-);
-const openMenu = document.querySelector(
-  '.icon-mobile-nav[name="menu-outline"]'
-);
-const header = document.querySelector(".header");
+// Show current year
+const currentYear = new Date().getFullYear();
+document.querySelector(".year").textContent = currentYear;
 
-const toggleNav = (action) => header.classList[action]("nav-open");
+// Showing menu in small devices
+// const closeMenu = document.querySelector(
+//   '.icon-mobile-nav[name="close-outline"]'
+// );
+// const openMenu = document.querySelector(
+//   '.icon-mobile-nav[name="menu-outline"]'
+// );
+// const header = document.querySelector(".header");
 
-closeMenu.addEventListener("click", () => toggleNav("remove"));
-openMenu.addEventListener("click", () => toggleNav("add"));
+// const toggleNav = (action) => header.classList[action]("nav-open");
+
+// closeMenu.addEventListener("click", () => toggleNav("remove"));
+// openMenu.addEventListener("click", () => toggleNav("add"));
+
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
+
+///////////////////////////////////////////////////////////
+// Smooth scrolling annimation
+const allLinks = document.querySelectorAll("a:link");
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // Scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    // Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const section = document.querySelector(href);
+      section.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // Close mobile navigation
+    if (link.classList.contains("nav-link")) {
+      headerEl.classList.toggle("nav-open");
+      // console.log("Hwy");
+    }
+  });
+});
+
+// Sticky navigation
+const sectionHeroEl = document.querySelector(".section-hero");
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    } else {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
+
+///////////////////////////////////////////////////////////
+// Fixing flexbox gap property missing in some Safari versions
+function checkFlexGap() {
+  var flex = document.createElement("div");
+  flex.style.display = "flex";
+  flex.style.flexDirection = "column";
+  flex.style.rowGap = "1px";
+
+  flex.appendChild(document.createElement("div"));
+  flex.appendChild(document.createElement("div"));
+
+  document.body.appendChild(flex);
+  var isSupported = flex.scrollHeight === 1;
+  flex.parentNode.removeChild(flex);
+  console.log(isSupported);
+
+  if (!isSupported) document.body.classList.add("no-flexbox-gap");
+}
+checkFlexGap();
