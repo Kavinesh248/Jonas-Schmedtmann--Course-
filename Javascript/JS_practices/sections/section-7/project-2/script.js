327c5b9e0651345f4fbe8b21ce3365053041a5b0
kavinesh <kavineshkavinesh860@gmail.com>
2024-07-04 16:49:09 +0530

    section2 project 2

---
+++ Javascript/JS_practices/sections/section-7/project-2/script.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const btnShowModal = document.querySelectorAll('.show-modal');
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnShowModal.forEach(function (el) {
+  el.addEventListener('click', openModal);
+});
+btnCloseModal.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  console.log(e.key);
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
