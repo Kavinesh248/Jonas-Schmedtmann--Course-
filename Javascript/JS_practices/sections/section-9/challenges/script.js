1bbea90a4cb665ee76fa5eca1d1f5f4debbbc1d6
kavinesh <kavineshkavinesh860@gmail.com>
2024-07-09 19:11:36 +0530

    course challenges

--- Javascript/JS_practices/sections/section-9/challenges/script.js
+++ Javascript/JS_practices/sections/section-9/challenges/script.js
@@ -80,7 +80,7 @@ const game = {
   scored: ["Lewandowski", "Gnarby", "Lewandowski", "Hummels"],
   date: "Nov 9th, 2037",
   odds: {
-    team1: 9.33,
+    team1: 1.33,
     x: 3.25,
     team2: 6.5,
   },
@@ -100,14 +100,128 @@ const player1Final = [...player1, "Thiago", "Coutinho", "Perisic"];
 
 // 5.
 const { team1, x: draw, team2 } = game.odds;
-console.log(team1, draw, team2);
+// console.logs(team1, draw, team2);
 
 // 6.
 const printGoals = function (...players) {
-  console.log(`${players.length} goals were scored`);
+  // console.log(`${players.length} goals were scored`);
 };
 printGoals(...game.scored);
 
 // 7.
 const lowerstOdd = team1 < team2 && `Team 1 is more likely to win!`;
-console.log(lowerstOdd);
+// console.log(lowerstOdd);
+
+/*
+Challenge 2:
+
+Let's continue with our football betting app!
+
+1. Loop over the game.scored array and print each
+   player name to the console, along with the goal
+   number (Example: "Goal 1: Lewandowski")
+
+2. Use a loop to calculate the average odd and log it
+   to the console (We already studied how to calculate
+   averages, you can go check if you don't remember)
+
+3. Print the 3 odds to the console, but in a nice
+    formatted way, exaclty like this:
+    Odd of victory Bayern Munich: 1.33
+    Odd of draw: 3.25
+    Odd of victory Borrussia Dortmund: 6.5
+    Get the team names directly from the game object,
+    don't hardcode them (except for "draw"). HINT: Note
+    how the odds and the game objects have the same
+    property names
+
+BONUS: Create an object called 'scorers' which
+      contains the names of the players who scored as
+      properties, and the number of goals as the value. In
+      this game, it will look like this:
+   
+      {
+        Gnarby: 1,
+        Hummels: 1,
+        Lewandowski: 2,
+      };
+*/
+
+const scored = game.scored;
+for (const [goal, player] of scored.entries()) {
+  // console.log(`Goal ${goal + 1}: ${player}`);
+}
+
+const odds = Object.values(game.odds);
+// let average = 0;
+// for (const team of odds) average += team;
+// average /= odds.length;
+// console.log(average);
+
+const finalOdds = Object.entries(game.odds);
+for (const [team, odd] of finalOdds) {
+  const openStr = team === "x" ? "draw" : `victory ${game[team]}`;
+  // console.log(`Odd of ${openStr}: ${odd}`);
+}
+// console.log(game["team1"]);
+// console.dir(game["team1"]);
+
+/*
+    Let's continue with our football betting app! This
+    time, we have a map with a log of the events that
+    happened during the game. The values are the events
+    themselves, and the keys are the minutes in which
+    each event happened (a football game has 90 minutes
+    plus some extra time).
+
+    1. Create an array 'events' of the different game
+    events that happened (no duplicates)
+
+    2. After the game has finished, is was found that the
+    yellow card from minute 64 was unfair. So remove this
+    event from the game events log.
+
+    3. Print the following string to the console: "An
+    event happened, on average, every 9 minutes" (keep in
+    mind that a game has 90 minutes)
+    
+    4. Loop over the events and log them to the console,
+    marking whether it's in the first half or second half
+    (after 45 min) of the game, like this:
+    [FIRST HALF] 17: ⚽ GOAL
+    GOOD LUCK
+*/
+
+const gameEvents = new Map([
+  [17, "⚽ GOAL"],
+  [36, "🔁 Substitution"],
+  [47, "⚽ GOAL"],
+  [61, "🔁 Substitution"],
+  [64, "🔶 Yellow card"],
+  [69, "🔴 Red card"],
+  [70, "🔁 Substitution"],
+  [72, "🔁 Substitution"],
+  [76, "⚽ GOAL"],
+  [80, "⚽ GOAL"],
+  [92, "🔶 Yellow card"],
+]);
+
+// 1.
+const events = new Set(gameEvents.values());
+console.log(events);
+
+// 2.
+gameEvents.delete(61);
+console.log(gameEvents);
+
+//3.
+console.log(
+  `An event happened, on average, every ${90 / gameEvents.size} minutes`
+);
+
+// 4.
+for (const [key, value] of gameEvents) {
+  key <= 45
+    ? console.log(`[FIRST HALF] ${key}: ${value}`)
+    : console.log(`[SECOND HALF] ${key}: ${value}`);
+}
