b704bc03194dd44d1af93b3d6458963739a2a016
kavinesh <kavineshkavinesh860@gmail.com>
2024-07-22 18:59:14 +0530

    static method

---
+++ Javascript/JS_practices/sections/section-14/static_method/script.js
@@ -0,0 +1,44 @@
+"use strict";
+
+class PersonCl {
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
+    this.birthYear = birthYear;
+  }
+
+  calcAge() {
+    console.log(2024 - this.birthYear);
+  }
+
+  get age() {
+    return 2024 - this.birthYear;
+  }
+
+  set fullName(name) {
+    if (name.includes(" ")) this._fullName = name;
+    else alert("It is not a full name!");
+  }
+}
+
+const jessica = new PersonCl("Jessica", 1996);
+// jessica.fullName = "Jessica Davis";
+console.log(jessica);
+
+jessica.calcAge();
+
+const account = {
+  owner: "Jonas",
+  movements: [200, 350, 900, 850],
+
+  get latest() {
+    return this.movements.slice(-1).pop();
+  },
+
+  set latest(mov) {
+    this.movements.push(mov);
+  },
+};
+
+console.log(account.latest);
+account.latest = 50;
+console.log(account);
