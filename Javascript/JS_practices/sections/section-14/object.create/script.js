0bf60bf1e32df1c6b6a7458f96a5440d73d46b6c
kavinesh <kavineshkavinesh860@gmail.com>
2024-07-23 18:56:44 +0530

    object.create

---
+++ Javascript/JS_practices/sections/section-14/object.create/script.js
@@ -0,0 +1,60 @@
+"use strict";
+
+class PersonCl {
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
+    this.birthYear = birthYear;
+  }
+
+  calcAge() {
+    console.log(2024 - this.birthYear);
+  }
+
+  get age() {
+    return 2024 - this.birthYear;
+  }
+
+  set fullName(name) {
+    if (name.includes(" ")) this._fullName = name;
+    else alert("It is not a full name!");
+  }
+}
+
+const jessica = new PersonCl("Jessica Davis", 1996);
+// jessica.fullName = "Jessica Davis";
+console.log(jessica);
+
+jessica.calcAge();
+
+const account = {
+  owner: "Jonas",
+  movements: [200, 350, 900, 850],
+
+  get latest() {
+    return this.movements.slice(-1).pop();
+  },
+
+  set latest(mov) {
+    this.movements.push(mov);
+  },
+};
+
+PersonCl.some = "Kavinesh";
+
+console.log(jessica.some);
+
+console.log(account.latest);
+account.latest = 50;
+console.log(account);
+
+const PersonProto = {
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  },
+};
+
+const steven = Object.create(PersonProto);
+steven.name = "steven";
+steven.birthYear = 2002;
+console.log(steven);
+steven.calcAge();
