720b147d193479d89ff9c6d9628639eb85ce1638
kavinesh <kavineshkavinesh860@gmail.com>
2024-07-30 19:12:34 +0530

    section challenges

--- Javascript/JS_practices/sections/section-16/challenges/script.js
+++ Javascript/JS_practices/sections/section-16/challenges/script.js
@@ -97,6 +97,101 @@ const whereAmI = function (lat, lng) {
     .finally(() => (countriesContainer.style.opacity = 1));
 };
 
-whereAmI(52.508, 13.381);
+// whereAmI(52.508, 13.381);
 // whereAmI(19.037, 72.873);
 // whereAmI(-33.933, 18.474);
+
+/* 
+
+    Build the image loading functionality that I just
+    showed you on the screen.
+
+    Tasks are not super-descriptive this time, so tht
+    you can figure out some stuff on your own. Pretend 
+    you're working on your own.
+
+    PART 1
+    1. Create a function 'createImage' which receives
+    imgPath as a input. This function returns a  promise
+    which creates a new image (use document.createElement)
+    and sets the .src attribute to the provided image paht.
+    When the image is done loading, append it to the
+    DOM element with the 'images' class, and resolve the
+    promise The fulfilled value should be the image
+    element itself. In case there is an error loading the image
+    ('error' event), reject the promise.
+
+    If this part is too tricky to you, just watch the
+    first part of the solution.
+
+    PART 2
+    2. Consume the promise using .then and also add an error
+    handler;
+    3. After the image has loaded, pause execution for 2 seconds using
+    the wait function that we created earlier; 
+    4. After 2 seconds have passed, hide the current image
+    (set display to none), and load a second image (hint: Use the
+    image element returned by the createImage promise to hide the current
+    image, you will need a global variable for that);
+    5. After the second image has loaded, pause execution for 2 seconds
+    again;
+    6. After the 2 seconds have passed, hide the current image.
+
+    TEST DATA: Images in the img folder. Test the error
+    handler by passing a wrong image path. Set the network speed
+    to 'Fast 3G' in the dev tools Network tab, otherwise
+    images load too fast.
+
+
+*/
+
+const createImage = function (imgPath) {
+  return new Promise(function (resolve, reject) {
+    const img = document.createElement("img");
+    img.src = imgPath;
+
+    img.addEventListener("load", function () {
+      document.querySelector(".images").append(img);
+      resolve(img);
+    });
+
+    img.addEventListener("error", function () {
+      reject(new Error("Image not found"));
+    });
+  });
+};
+
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+
+let currentImg;
+
+createImage("img/img-1.jpg")
+  .then((res) => {
+    currentImg = res;
+    return wait(2);
+  })
+  .then((res) => {
+    currentImg.style.display = "none";
+    return createImage("img/img-2.jpg");
+  })
+  .then((res) => {
+    currentImg = res;
+    return wait(2);
+  })
+  .then((res) => {
+    currentImg.style.display = "none";
+    return createImage("img/img-3.jpg");
+  })
+  .then((res) => {
+    currentImg = res;
+    return wait(2);
+  })
+  .then((res) => {
+    currentImg.style.display = "none";
+  })
+
+  .catch((err) => console.error(err));
