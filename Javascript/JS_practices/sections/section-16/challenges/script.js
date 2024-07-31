30ec00ed5f4e5e9b9ed2a55e1b25d619a63fc67b
kavinesh <kavineshkavinesh860@gmail.com>
2024-07-31 17:12:53 +0530

    section challenges

--- Javascript/JS_practices/sections/section-16/challenges/script.js
+++ Javascript/JS_practices/sections/section-16/challenges/script.js
@@ -162,36 +162,91 @@ const createImage = function (imgPath) {
 };
 
 const wait = function (seconds) {
-  return new Promise(function (resolve) {
-    setTimeout(resolve, seconds * 1000);
-  });
+  return new Promise((resolve) => setTimeout(resolve, seconds * 1000));
 };
 
 let currentImg;
 
-createImage("img/img-1.jpg")
-  .then((res) => {
-    currentImg = res;
-    return wait(2);
-  })
-  .then((res) => {
-    currentImg.style.display = "none";
-    return createImage("img/img-2.jpg");
-  })
-  .then((res) => {
-    currentImg = res;
-    return wait(2);
-  })
-  .then((res) => {
-    currentImg.style.display = "none";
-    return createImage("img/img-3.jpg");
-  })
-  .then((res) => {
-    currentImg = res;
-    return wait(2);
-  })
-  .then((res) => {
-    currentImg.style.display = "none";
-  })
+// createImage("img/img-1.jpg")
+//   .then((res) => {
+//     currentImg = res;
+//     return wait(2);
+//   })
+//   .then((res) => {
+//     currentImg.style.display = "none";
+//     return createImage("img/img-2.jpg");
+//   })
+//   .then((res) => {
+//     currentImg = res;
+//     return wait(2);
+//   })
+//   .then((res) => {
+//     currentImg.style.display = "none";
+//     return createImage("img/img-3.jpg");
+//   })
+//   .then((res) => {
+//     currentImg = res;
+//     return wait(2);
+//   })
+//   .then((res) => {
+//     currentImg.style.display = "none";
+//   })
 
-  .catch((err) => console.error(err));
+// .catch((err) => console.error(err));
+
+/*
+
+  PART 1
+  
+  Write an async function "loadNPause" that recreates Coding
+  Challenge 2, this time using async/await (only the
+  part where the promise is consumed). Compare the two versions,
+  think about the big differences, and see which
+  one you like more. Don't forget to test error handler, 
+  and to set the networrk speed to 'Fast 3G' in the
+  dev tools Network tab.
+
+  PART 2
+  1. Create an async function 'loadAll' that
+   receives an array of image path
+  'imgArr' 
+  2. Use .map to loop over the arrya, to all the images wit the
+  'createImage' function (call the resulting array 'imgs')
+  3. Check out the 'imgs' array in the console! Is it like you 
+  expected?
+  4. Use a promise combinator function to actually 
+  get the images
+  from the array
+  5. Add the 'parallel' class to all the images 
+  (it has some Css style)
+
+  TEST DATA:  ['img/img-1.jpg', 'img/img-1.jpg', 'img/img-1.jpg']
+  . To test, turn off the 'loadNPause' function
+
+
+  GOOD LUCK 🐦‍🔥
+*/
+
+const loadNPause = async function () {
+  try {
+    let img = await createImage("img/img-1.jpg");
+    console.log("Image 1 has loaded");
+
+    // Wait for 2 seconds and log the message
+    await wait(2);
+    img.style.display = "none";
+
+    img = await createImage("img/img-2.jpg");
+    console.log("Image 2 has loaded");
+    await wait(2);
+    img.style.display = "none";
+
+    img = await createImage("img/img-3.jpg");
+    console.log("Image 3 has loaded");
+    await wait(2);
+  } catch (err) {
+    console.error(err.message);
+  }
+};
+
+loadNPause();
