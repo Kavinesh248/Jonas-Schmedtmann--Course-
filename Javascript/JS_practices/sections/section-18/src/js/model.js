a4353e17d9f3f0fadd155943d4a3d00a1b2f2602
kavinesh <kavineshkavinesh860@gmail.com>
2024-08-05 19:21:38 +0530

    model file

---
+++ Javascript/JS_practices/sections/section-18/src/js/model.js
@@ -0,0 +1,27 @@
+import { async } from 'regenerator-runtime';
+import { API_URL } from './config.js';
+import { getJSON } from './helpers.js';
+
+export const state = {
+  recipe: {},
+};
+
+export const loadRecipe = async function (id) {
+  try {
+    const data = await getJSON(`${API_URL}/${id}`);
+    const { recipe } = data.data;
+
+    state.recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      image: recipe.image_url,
+      sourceUrl: recipe.source_url,
+      servings: recipe.servings,
+      ingredients: recipe.ingredients,
+      cookingTime: recipe.cooking_time,
+    };
+  } catch (err) {
+    console.error(err);
+  }
+};
