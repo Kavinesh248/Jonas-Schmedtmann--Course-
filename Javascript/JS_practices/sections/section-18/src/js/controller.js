61dfc4f4c0ac71b4ef23f6b74957fa909149cb28
kavinesh <kavineshkavinesh860@gmail.com>
2024-08-07 19:44:33 +0530

    main controller js file

--- Javascript/JS_practices/sections/section-18/src/js/controller.js
+++ Javascript/JS_practices/sections/section-18/src/js/controller.js
@@ -5,6 +5,11 @@ import * as model from './model.js';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
+import paginationView from './views/paginationView.js';
+
+// if (module.hot) {
+//   module.hot.accept();
+// }
 
 const controlRecipes = async function () {
   try {
@@ -34,15 +39,23 @@ const controlSearchResults = async function () {
 
     resultsView.generateMarkupSpinner();
 
-    resultsView.render(model.state.search.results);
+    resultsView.render(model.getSearchResultsPage(4));
+
+    paginationView.render(model.state.search);
   } catch (err) {
     console.log(err);
   }
 };
 
+const controlPagination = function (goToPage) {
+  resultsView.render(model.getSearchResultsPage(goToPage));
+  paginationView.render(model.state.search);
+};
+
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
   searchView.addHandlerSearch(controlSearchResults);
+  paginationView.addHanlerClick(controlPagination);
 };
 
 init();
