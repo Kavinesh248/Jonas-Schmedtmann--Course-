ed8fdf6ae288bd9d29a7665defb6c5bace8df37e
kavinesh <kavineshkavinesh860@gmail.com>
2024-08-08 18:45:19 +0530

    view js file

--- Javascript/JS_practices/sections/section-18/src/js/views/View.js
+++ Javascript/JS_practices/sections/section-18/src/js/views/View.js
@@ -6,7 +6,6 @@ export default class View {
   render(data) {
     if (!data || (Array.isArray(data) && data.length === 0))
       return this.renderError();
-
     this._data = data;
     const markup = this._generateMarkup();
     this._clear();
@@ -16,6 +15,33 @@ export default class View {
   // Private Methods
   _clear() {
     this._parentElement.innerHTML = '';
+  }
+
+  update(data) {
+    this._data = data;
+    const newMarkup = this._generateMarkup();
+
+    const newDom = document.createRange().createContextualFragment(newMarkup);
+    const newElements = Array.from(newDom.querySelectorAll('*'));
+    const curElements = Array.from(this._parentElement.querySelectorAll('*'));
+
+    newElements.forEach((newEl, i) => {
+      const curEl = curElements[i];
+
+      // Updates changed TEXT
+      if (
+        !newEl.isEqualNode(curEl) &&
+        newEl.firstChild?.nodeValue.trim() !== ''
+      ) {
+        curEl.textContent = newEl.textContent;
+      }
+
+      // Updates changed ATTRIBUTES
+      if (!newEl.isEqualNode(curEl))
+        Array.from(newEl.attributes).forEach(attr =>
+          curEl.setAttribute(attr.name, attr.value)
+        );
+    });
   }
 
   renderError(message = this._errorMessage) {
