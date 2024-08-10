41934443758b6d54a5e79914834239d370a1d208
kavinesh <kavineshkavinesh860@gmail.com>
2024-08-10 19:19:33 +0530

    configuration file

--- Javascript/JS_practices/sections/section-18/src/js/config.js
+++ Javascript/JS_practices/sections/section-18/src/js/config.js
@@ -1,2 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
-export const TIMOUT_SEC = 10;
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '4a0b2303-a992-4922-9180-bf0043883b58';
+export const MODAL_CLOSE_SEC = 2.5;
