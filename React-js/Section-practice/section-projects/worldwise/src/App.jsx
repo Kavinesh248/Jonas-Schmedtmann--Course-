4729d5c24d195810286142641f586ebb19db6669
kavinesh <kavineshkavinesh860@gmail.com>
2024-10-12 22:23:38 +0530

    app js file

--- React-js/Section-practice/section-projects/worldwise/src/App.jsx
+++ React-js/Section-practice/section-projects/worldwise/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import Pricing from "./pages/Pricing";
 import Product from "./pages/Product";
@@ -7,6 +7,9 @@ import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
 import { useEffect, useState } from "react";
 import CityList from "./components/City/CityList";
+import CountryList from "./components/Country/CountryList";
+import City from "./components/City/City";
+import Form from "./components/Form/Form";
 
 function App() {
   const [cities, setCities] = useState([]);
@@ -38,16 +41,17 @@ function App() {
         <Route path="pricing" element={<Pricing />} />
         <Route path="/login" element={<Login />} />
         <Route path="app" element={<AppLayout />}>
-          <Route
-            index
-            element={<CityList cities={cities} isLoading={isLoading} />}
-          />
+          <Route index element={<Navigate replace to="cities" />} />
           <Route
             path="cities"
             element={<CityList cities={cities} isLoading={isLoading} />}
           />
-          <Route path="countries" element={<p>List of countries</p>} />
-          <Route path="form" element={<p>Form</p>} />
+          <Route
+            path="countries"
+            element={<CountryList cities={cities} isLoading={isLoading} />}
+          />
+          <Route path="cities/:id" element={<City />} />
+          <Route path="form" element={<Form />} />
         </Route>
         <Route path="*" element={<PageNotFound />} />
       </Routes>
