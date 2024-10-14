d6bee0595a41d456f3b060de76c8b742698aa964
kavinesh <kavineshkavinesh860@gmail.com>
2024-10-14 22:22:49 +0530

    cities context file

---
+++ React-js/Section-practice/section-projects/worldwise/src/contexts/CitiesContext.jsx
@@ -0,0 +1,69 @@
+import { useContext } from "react";
+import { useState, useEffect } from "react";
+import { createContext } from "react";
+
+const CitiesContext = createContext();
+
+function CitiesProvider({ children }) {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [currentCity, setCurrentCity] = useState({});
+
+  const BASE_URL = "http://localhost:9000";
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch (err) {
+        console.log(err.message);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+
+  async function getCity(id) {
+    try {
+      setIsLoading(true);
+      const res = await fetch(`${BASE_URL}/cities/${id}`);
+
+      if (!res.ok) {
+        throw new Error(`City with ID ${id} not found.`);
+      }
+
+      const data = await res.json();
+      setCurrentCity(data);
+    } catch (err) {
+      console.log(err.message);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  return (
+    <CitiesContext.Provider
+      value={{
+        cities,
+        isLoading,
+        currentCity,
+        getCity,
+      }}
+    >
+      {children}
+    </CitiesContext.Provider>
+  );
+}
+
+function useCities() {
+  const context = useContext(CitiesContext);
+  if (context === undefined)
+    throw new Error("CitiesContext was used by the CitiesProvider");
+  return context;
+}
+
+export { CitiesProvider, useCities };
